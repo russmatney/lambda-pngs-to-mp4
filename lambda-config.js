1b8ca2ad654580cd247adbfc9aef346d938098af
Russell Matney <russell.matney@gmail.com>
2015-04-15 16:50:40 -0400

    lambda config formatting update

--- lambda-config.js
+++ lambda-config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  region: 'us-east-1',
-  handler: 'index.handler',
-  role: 'arn:aws:iam::106586740595:role/executionrole',
-  functionName: 'pngs-to-mp4',
-  timeout: 60
+  FunctionName: 'pngs-to-mp4',
+  Handler: 'index.handler',
+  Role: 'arn:aws:iam::106586740595:role/executionrole',
+  Region: 'us-east-1',
+  Runtime: 'nodejs',
+  Timeout: 60
 }
